9b781fa963c8b59d69f27ae673f87682105f3f98
Hassan Badru <hassanbadru@Hassans-MacBook-Air.local>
2018-06-03 21:39:04 -0400

    frontend setup for part2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './css/App.css';
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {operation: 'encoding', user_input: '', file_upload: false, result: ''}
+    this.state = {operation: 'encoding', user_input: '', file_upload: false, result: '', part: 2}
   }
 
 
@@ -25,27 +25,30 @@ class App extends Component {
                 <h1>ART +<span className={"brand-heading"}> LOGIC</span></h1>
                 <p className={"intro-text"}>Use this app to encode and decode data...</p>
               </div>
-             </div>
+            </div>
 
-             <div className={'row'}>
 
-               <UserInput
-                 toEncode={() => this.setState({operation: 'encoding'})}
-                 toDecode={() => this.setState({operation: 'decoding'})}
-                 user_input={(user_input) => this.setState({user_input})}
-                 currentOperation={this.state.operation}
-                 toUpload={() => this.setState({file_upload: !this.state.file_upload})}
-                 file_upload={this.state.file_upload}
-                 get_result={() => this.setState({result: window.result})}
-               />
+            {
+              (this.state.part === 1) ? (
+                <Part1
+                  toEncode={() => this.setState({operation: 'encoding'})}
+                  toDecode={() => this.setState({operation: 'decoding'})}
+                  user_input={(user_input) => this.setState({user_input})}
+                  currentOperation={this.state.operation}
+                  toUpload={() => this.setState({file_upload: !this.state.file_upload})}
+                  file_upload={this.state.file_upload}
+                  get_result={() => this.setState({result: window.result})}
 
-               <ResultDisplay
-                 currentOperation={this.state.operation}
-                 user_input={this.state.user_input}
-                 result={this.state.result}
+                  currentOperation={this.state.operation}
+                  user_input={this.state.user_input}
+                  result={this.state.result}
                 />
-             </div>
+              ) : (
+                <Part2 />
+              )
 
+
+            }
 
 
             </div>
@@ -56,7 +59,11 @@ class App extends Component {
   }
 }
 
-export default App;
+
+// const Part2 = (props) => {
+//
+// }
+
 
 const UserInput = (props) => {
 
@@ -65,46 +72,34 @@ const UserInput = (props) => {
   return (
 
     <div className={"form"}>
-        <form action="">
-          <h4>Select an operation</h4>
-          <span className={'message'}>
+      <form action="">
+        <h4>Select an operation</h4>
+        <span className={'message'}>
 
-              <div>
-                <label className={"radio-inline message"}>&nbsp;<input type="radio" name="encode" onChange={props.toEncode} checked={encodingStatus} />Encode</label>
-                <label className={"radio-inline message"}>&nbsp;<input type="radio" name="decode" onChange={props.toDecode} checked={!encodingStatus}/>Decode</label>
-              </div>
+          <div>
+            <label className={"radio-inline message"}>&nbsp;<input type="radio" name="encode" onChange={props.toEncode} checked={encodingStatus} />Encode</label>
+            <label className={"radio-inline message"}>&nbsp;<input type="radio" name="decode" onChange={props.toDecode} checked={!encodingStatus}/>Decode</label>
+          </div>
 
-          </span>
+        </span>
 
-          <br/>
-          {
-            (props.file_upload) ? (
-                    <div>
-                      <input
-                        type="file"
-                        name="file_compute"
-                      />
+        <br/>
 
-                      <p style={{color: '#000'}}>Prefer to type in? Use the<a onClick={props.toUpload} href="#" style={{color: 'orange'}}> Text Box</a></p>
-                      <button>Compute</button>
-                    </div>
-                  ) : (
-                    <div>
-                      <input
-                        type="text"
-                        placeholder={(encodingStatus) ? 'Enter Decimal Value' : 'Enter Hexadecimal Value'}
-                        name="to_compute"
-                        onChange={(e) => {
-                          props.user_input(e.target.value)
-                          console.log(e.target.value)
-                        }}
-                      />
-                      {/* <p style={{color: '#000'}}>Have a text file? <a onClick={props.toUpload} href="#" style={{color: 'orange'}}>Upload</a> instead</p> */}
+            <div>
+              <input
+                type="text"
+                placeholder={(encodingStatus) ? 'Enter Decimal Value' : 'Enter Hexadecimal Value'}
+                name="to_compute"
+                onChange={(e) => {
+                  props.user_input(e.target.value)
+                  console.log(e.target.value)
+                }}
+              />
+              {/* <p style={{color: '#000'}}>Have a text file? <a onClick={props.toUpload} href="#" style={{color: 'orange'}}>Upload</a> instead</p> */}
 
-                      <button type="submit" onClick={props.get_result}> Compute</button>
-                    </div>
-                  )
-          }
+              <button type="submit" onClick={props.get_result}> Compute</button>
+            </div>
+
 
           <br/>
 
@@ -151,13 +146,66 @@ const ResultDisplay = (props) => {
   )
 }
 
-//
-// <div className="App">
-//   <header className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//     <h1 className="App-title">Welcome to React</h1>
-//   </header>
-//   <p className="App-intro">
-//     To get started, edit <code>src/App.js</code> and save to reload.
-//   </p>
-// </div>
+const Part1 = (props) => {
+  return (
+    <div className={'row'}>
+      <UserInput
+        toEncode={props.toEncode}
+        toDecode={props.toDecode}
+        user_input={props.user_input}
+        currentOperation={props.currentOperation}
+        toUpload={props.toUpload}
+        file_upload={props.file_upload}
+        get_result={props.get_result}
+      />
+
+      <ResultDisplay
+        currentOperation={props.operation}
+        user_input={props.user_input}
+        result={props.result}
+      />
+    </div>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <div style={{backgroundColor: '#fff', paddingTop: 30}}>
+      <h4>Entire desired instruction</h4>
+      <br />
+      <UserInput2 />
+    </div>
+  )
+}
+
+
+const UserInput2 = (props) => {
+
+  return (
+
+    <div className="form">
+      <form action="">
+
+
+        <input
+          type="text"
+          placeholder='Instruction'
+          name="to_compute"
+
+        />
+
+        <button type="submit"> Compute</button>
+
+      </form>
+
+      {/* <a href={window.MEDIA_URL + '/ConvertedData.txt'}>
+        <i className={'message'} className="fas fa-download" > {'download ConvertedData.txt'} </i>
+      </a> */}
+
+    </div>
+  )
+}
+
+
+
+export default App;
